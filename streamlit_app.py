85ca955ba4f92c7b270031762c103b2ce3a78f7a
Marcin Molak <frodo2000@gmail.com>
2022-09-07 11:22:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom\'s New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bluberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorities')
+streamlit.text('🥣Omega 3 & Bluberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
